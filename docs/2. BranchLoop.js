99650c87d25a34385cd88dca0615cc9bb2ccca31
Andre Kristanto <akristan001@e.ntu.edu.sg>
2019-06-16 14:13:43 +0800

    [Docs] Add branch, looping, and statement + boolean

---
+++ docs/2. BranchLoop.js
@@ -0,0 +1,61 @@
+/**
+ * Welcome to Branch & Loop
+ *
+ * You will learn about the `if-else`, `for`, and `while` statement
+ */
+
+/**
+ * Syntax:
+ *
+ * [If-Else]
+ *
+ * if (statement1) {
+ *   ...
+ * } else {
+ *   ...
+ * }
+ *
+ * [If-ElseIf-Else]
+ *
+ * if (statement1) {
+ *   ...
+ * } else if (statement2) {
+ *   ...
+ * } else {
+ *   ...
+ * }
+ *
+ * [If-ElseIf]
+ *
+ * if (statement1) {
+ *   ...
+ * } else if (statement2) {
+ *   ...
+ * }
+ *
+ * [For]
+ *
+ * for (variable; condition; after-loop) {
+ *   ...
+ * }
+ *
+ * [While]
+ *
+ * while (statement) {
+ *   ...
+ * }
+ */
+
+ /**
+  * Statement (has a boolean expression either true or false)
+  * 
+  * Operator (or known as Logical Operator): NOT (!), AND (&&), OR (||), XOR (^)
+  * 
+  * For logical operator results of True and False can be seen here:
+  * `https://introcs.cs.princeton.edu/java/71boolean/`
+  * 
+  * For example:
+  * let exBool = true
+  * console.log(!exBool)
+  * console.log(exBool && false)
+  */
