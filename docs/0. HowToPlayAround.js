1bb227d95fba85eb635be4a3c5680747dadabb28
Andre Kristanto <akristan001@e.ntu.edu.sg>
2019-06-16 14:12:50 +0800

    [Docs] Add How to play around with Node

---
+++ docs/0. HowToPlayAround.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Install Node.js from `https://nodejs.org/en/`
+ * 2. Open your terminal and type in `node`
+ * 3. The command you just run will put you in the Node Engine using JavaScript environment
+ * 4. You can play around with basic syntax to understand Chapter 1
+ * 5. You can also run the JavaScript file (ending with `.js` extension) by running `node <your javascript file>`
+ */
